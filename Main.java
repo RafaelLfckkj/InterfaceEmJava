3990225cee2cbacf3fdb2311b194e5689afbe18c
RafaelLfckkj <rafaxolive@gmail.com>
2026-06-06 13:41:03 -0300

    parte 2 da resolução do problema sem Interface

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import entities.BrazilTaxService;
 import entities.CarRental;
+import entities.RentalService;
 import entities.Vehicle;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -14,18 +15,29 @@ public class Main {
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
         System.out.println("Entre com dados do Aluguel: ");
-        System.out.print("Modelo Do carro:");
+        System.out.print("Modelo Do carro: ");
         String carModel = Scan.nextLine();
-        System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
+        System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
         LocalDateTime start = LocalDateTime.parse(Scan.nextLine(), fmt);
-        System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
+        System.out.print("Retorno (dd/MM/yyyy hh:mm): ");
         LocalDateTime fisish = LocalDateTime.parse(Scan.nextLine(), fmt);
 
         CarRental cr = new CarRental(start, fisish, new Vehicle(carModel));
 
-        BrazilTaxService taxService = new BrazilTaxService();
-        System.out.println(taxService.tax(50.0));
+        System.out.print("Entre com o preoço por hora: ");
+        double pricePerHour = Scan.nextDouble();
+        System.out.print("Entre com o preoço por dia: ");
+        double pricePerDay = Scan.nextDouble();
 
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+
+        rentalService.processInvoice(cr);
+
+        System.out.println("Fatura: ");
+        System.out.println("Pagamento Basico: " + cr.getInvoice().getBasicPayment());
+        System.out.println("Imposto: " + cr.getInvoice().getTax());
+        System.out.println("Pagamento Total: " + cr.getInvoice().getTotalPayment());
+        
         Scan.close();
     }
 }
--- entities/Invoice.java
+++ entities/Invoice.java
@@ -30,7 +30,7 @@ public class Invoice {
     }
 
     public double getTotalPayment(){
-        return getBasicPayment() - getTax();
+        return getBasicPayment() + getTax();
     }
 }
 
